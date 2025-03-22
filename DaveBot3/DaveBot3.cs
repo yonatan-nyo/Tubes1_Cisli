fc1c5775586db055b61f42a4ba8699908f7cf590
Dave <d06163606@gmail.com>
2025-03-22 20:41:40 +0700

    update dave bot

--- DaveBot3/DaveBot3.cs
+++ DaveBot3/DaveBot3.cs
@@ -4,8 +4,6 @@ using Robocode.TankRoyale.BotApi.Events;
 using System.Drawing;
 using System.IO;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Configuration.Json;
-using System.Collections.Generic;
 using System;
 
 public class DaveBot3 : Bot
@@ -33,26 +31,52 @@ public class DaveBot3 : Bot
 
         while (IsRunning)
         {
-            SetTurnLeft(100);
+            SetTurnLeft(90);
             Forward(100);
+            TurnGunLeft(10);
+        }
+    }
+
+    private double getFirePower(double x, double y){
+        double distance = DistanceTo(x, y);
+        if(Energy < 10){
+            return 0.1;
+        }
+        if(distance <10){
+            return 50;
+        }
+        if (distance < 200){
+            return 3;
+        } else if (distance < 300){
+            return 2.5;
+        }  else if (distance < 400){
+            return 2;
+        }else if (distance < 500){
+            return 1.5;
+        } else {
+            return 1;
+        }
+    }
+    
+    private double getAngleAdjustMent(double bearingFromGun){
+        if(Energy < 10){
+            return 0.1;
+        }
+        if (bearingFromGun < 45){
+            return 0;
+        } else if (bearingFromGun < 90){
+            return 10;
+        } else if (bearingFromGun < 145){
+            return 25;
+        } else{
+            return 37;
         }
     }
 
     public override void OnScannedBot(ScannedBotEvent e)
     {
-        var distance = DistanceTo(e.X, e.Y);
-        if (distance > 500)
-        {
-            Fire(1);
-        }
-        else if (distance > 300)
-        {
-            Fire(2);
-        }
-        else
-        {
-            Fire(3);
-        }
+        double bulletPower = getFirePower(e.X, e.Y);
+        Fire(bulletPower);
     }
 
     public override void OnHitBot(HitBotEvent e)
@@ -83,27 +107,17 @@ public class DaveBot3 : Bot
         public override void OnBulletHit(BulletHitBotEvent e)
     {
         var bearingFromGun = GunBearingTo(e.Bullet.X, e.Bullet.Y);
-        var distance = DistanceTo(e.Bullet.X, e.Bullet.Y);
+        double angleAdjustment=getAngleAdjustMent(bearingFromGun);
+
         if (bearingFromGun <= 0)
         {
-            TurnGunRight(Math.Abs(bearingFromGun));
+            TurnGunRight(Math.Abs(bearingFromGun)+angleAdjustment);
         }
         else
         {
-            TurnGunLeft(Math.Abs(bearingFromGun));
+            TurnGunLeft(Math.Abs(bearingFromGun)+angleAdjustment);
         }
-
-        if (distance > 500)
-        {
-            Fire(1);
-        }
-        else if (distance > 300)
-        {
-            Fire(2);
-        }
-        else
-        {
-            Fire(3);
-        }
+        double bulletPower = getFirePower(e.Bullet.X, e.Bullet.Y);
+        Fire(bulletPower);
     }
 }
